bba0f9a0d20fc320db18019d6a46c9cfc75a8e0e
kartikmishraa <kartikm2219@gmail.com>
2023-06-13 14:15:34 +0530

    implemented a route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,19 @@
+const asyncHandler = require("express-async-handler");
+
+// @desc: Send back a simple welcome message
+// @route: GET /api/welcome
 const welcomeMsg = (req, res) => {
-  res.json({
+  res.status(200).json({
     success: true,
     message: "API successfully called",
   });
 };
 
-module.exports = { welcomeMsg };
+// @desc:
+// @route: POST /api/signup
+const signup = asyncHandler(async (req, res) => {
+  const { name, email, password, phone } = req.body;
+  console.log(name, email, password, phone);
+});
+
+module.exports = { welcomeMsg, signup };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const controller = require("../controllers/userController");
 
 router.get("/welcome", controller.welcomeMsg);
+router.post("/signup", controller.signup);
 
 module.exports = router;
