dfe01a190a9130b9d96f68382f4f99091937dd07
kartikmishraa <kartikm2219@gmail.com>
2023-06-14 19:51:33 +0530

    id --> user_id

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,11 @@ class User extends Model {}
 
 User.init(
   {
+    user_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
     },
